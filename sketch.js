de68a14e4e68f7b1dd24a272e5d10b29d577a6a7
zz-22-t <161503379+zz-22-t@users.noreply.github.com>
2024-05-02 09:00:43 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,24 +1,64 @@
 
-let r = 25;
-let angle = 0;
-let font2,points;[]
-function preload() {
-  font2= loadFont("assets/Cormorant-Italic-VariableFont_wght.ttf")
+var col = {
+  r: 0,
+  g: 0,
+  b: 0,
 }
 
+let r = 10;
+let angle = 0;
+let font;
+let points;[];
+
+function preload() {
+  font= loadFont('assets/NanumGothicCoding-Bold.ttf')
+}
 function setup() {
   createCanvas(400, 400);
-  textFont(font2);
-  textSize(20);
+  col.r = random(0, 255);
+  col.g = random(0, 255);
+  col.b = random(0, 255);
+  noStroke();
+  fill(col.r, col.g, col.b);
+  textSize(10);
+  textFont(font);
+  
+  points = font.textToPoints("ZAIRA TORRES",5,200,200 ,{
+    sampleFactor:0.2,
+    simplifyThreshold:0
+  }); 
+print(points);
+
+  angleMode(DEGREES);
 }
+
 
 function draw() {
-  background(220);
-  points = font2.textToPoints("ZT",50,200,300)
+  background(0);
+  let born_year = 1995;
+  let current_year = year();
+  let age_years = (current_year - 1) - born_year;
+  text(age_years + " years old", 20,30);
+  
+  let birth_date = new Date(1995,8,28);
+  let time_passed_ms = new Date() - birth_date
 
-for(let i=0; i<points.length;i++){
-ellipse(points[i].x+r*sin(angle + i*25),points[i].y,10,10);
+  let days_passed = time_passed_ms / (24*60*60*1000);
+  let truncated_days_passed = nf(days_passed, 0, 5
+    );
+  text(truncated_days_passed + " days passed since birth", 20, 60)
+
+  for (let i = 0; i < points.length; i++) {
+    ellipse(points[i].x+r*sin(angle + i*25),points[i].y,5,5);
+    }
+
+    angle += 10;
+  
 }
- 
-print(points)
+
+function mousePressed() {
+  col.r= random(0,255);
+  col.g= random(0,255);
+  col.b= random(0,255);
+  fill(col.r, col.g, col.b);
 }
